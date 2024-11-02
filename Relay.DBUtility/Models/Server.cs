0fb567c3e45806b9e755f4a12d42b9b50426f43e
krHuda <krhuda@yandex.ru>
2024-11-02 12:40:28 +0300

    Added base server

---
+++ Relay.DBUtility/Models/Server.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Relay.DBUtility.Models;
+
+public class Server : DbContext
+{
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Address { get; set; }
+    public int Port { get; set; }
+    public string? Password { get; set; }
+}
---
+++ Relay.Server/Models/Chat.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Relay.DBUtility;
+using Relay.DBUtility.Models;
+using Relay.DBUtility;
+using Relay.DBUtility.Models;
+
+namespace Relay.Server.Models;
+
+internal class Chat
+{
+    public required int Id { get; set; }
+    
+    public required string Name { get; set; }
+    
+    public IEnumerable<Message>? Messages { get; set; }
+    
+    public IEnumerable<User>? Users { get; set; }
+}
